683d0d108797b015cfcd8eedcd9a39607b2a4ae4
Miranto <85173375+mirantocodes@users.noreply.github.com>
2022-03-14 11:41:58 +0300

    Improve Post/Comment UI

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_11_151509) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_13_135331) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_11_151509) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "picture"
   end
 
   add_foreign_key "comments", "posts"
