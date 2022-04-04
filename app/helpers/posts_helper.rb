4ed4a010765d8d3385aeb370d07c8c33ce560e18
Miranto <85173375+mirantocodes@users.noreply.github.com>
2022-04-04 07:51:43 +0300

    Refactor Post with datetime

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,11 @@
 module PostsHelper
+  def time_ago_in_words_only_today(time)
+    if time > Time.now.beginning_of_day
+      time_ago_in_words time
+    elsif time > Time.now.beginning_of_year
+      time.strftime("%b %d")
+    else
+      time.strftime("%b %d %Y")
+    end
+  end
 end
