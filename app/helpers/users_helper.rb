9808d33c67e3951d86647764522daac8b4c2347f
Miranto <85173375+mirantocodes@users.noreply.github.com>
2022-04-05 07:45:20 +0300

    Not check anymore if user not nil or user.picture not nil

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,9 @@
 module UsersHelper
   def picture(user, width: 46)
-    unless user.nil?
-      if user.picture.nil?
-        link_to(image_tag('default-user.png', width: width, alt: "Default user profile picture"), user)
-      else
-        link_to(image_tag(user.picture, width: width, alt: "User profile picture", class: "rounded-circle"), user)
-      end
+    if user.picture
+      link_to(image_tag(user.picture, width: width, alt: 'User profile picture', class: 'rounded-circle'), user)
+    else
+      link_to(image_tag('default-user.png', width: width, alt: 'Default user profile picture', class: 'rounded-circle'), user)
     end
   end
 end
