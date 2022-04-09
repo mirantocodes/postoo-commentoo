b234f0d6db6b12d1c0b325e4ad022d29d0f137c9
Miranto <85173375+mirantocodes@users.noreply.github.com>
2022-04-09 10:09:58 +0300

    Fix issue for default comment picture when not logged in

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   def picture(user, width: 46)
-    if user.picture
+    if user&.picture
       link_to(image_tag(user.picture, width: width, alt: 'User profile picture', class: 'rounded-circle'), user)
     else
       link_to(image_tag('default-user.png', width: width, alt: 'Default user profile picture', class: 'rounded-circle'), user)
